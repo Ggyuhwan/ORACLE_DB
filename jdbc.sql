95c3b4123003a1857d5c158d0571327ff8598eab
rbghks4677 <rbghks4677@naver.com>
2023-09-13 21:19:44 +0900

    오라클 스프링 DB 연결23.09.13

--- jdbc.sql
+++ jdbc.sql
@@ -19,3 +19,35 @@ COMMIT;
 
 SELECT *
 FROM MEMBER;
+
+CREATE TABLE board(
+    board_no    NUMBER(10)     PRIMARY KEY
+    ,board_title VARCHAR2(1000)
+    ,board_content VARCHAR2(4000)
+    ,mem_id         VARCHAR2(100)
+    ,create_date    DATE DEFAULT SYSDATE
+    ,update_date    DATE DEFAULT SYSDATE
+    );
+ALTER TABLE board 
+ADD CONSTRAINT fk_board FOREIGN KEY(mem_id) REFERENCES member(mem_id);
+
+INSERT INTO board(  board_no, board_title, board_content, mem_id)
+VALUES((SELECT NVL(MAX(board_no),0) +1
+FROM board)
+    ,'공지사항'
+    ,'첫번째 글'
+    ,'USA');
+SELECT a.board_no
+    , a.board_title
+    , a.board_content
+    , b.mem_id
+    , b.mem_nm
+    , a.create_date
+    , a.update_date
+FROM board a, member b
+WHERE a.mem_id = b.mem_id
+ORDER BY a.update_date DESC;
+
+SELECT NVL(MAX(board_no),0) +1
+FROM board;
+commit;
